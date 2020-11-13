974d11b20d2db047fe70f3dbbaf09415be4d36bc
Andrii712 <andrew.zaharovskyi@gmail.com>
2020-11-13 09:24:12 +0200

    Code has been refactored
    - unused directives has been removed;
    - a display name for service has been added;

--- TemperatureLogger/Installer1.cs
+++ TemperatureLogger/Installer1.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Configuration.Install;
-using System.Linq;
 using System.ServiceProcess;
-using System.Threading.Tasks;
 
 namespace TemperatureLogger
 {
     [RunInstaller(true)]
-    public partial class Installer1 : System.Configuration.Install.Installer
+    public partial class Installer1 : Installer
     {
         readonly ServiceInstaller serviceInstaller;
         readonly ServiceProcessInstaller processInstaller;
@@ -18,12 +13,14 @@ namespace TemperatureLogger
         public Installer1()
         {
             InitializeComponent();
+
             processInstaller = new ServiceProcessInstaller();
             serviceInstaller = new ServiceInstaller();
 
             processInstaller.Account = ServiceAccount.LocalSystem;
             serviceInstaller.StartType = ServiceStartMode.Automatic;
             serviceInstaller.ServiceName = "TemperatureLogger";
+            serviceInstaller.DisplayName = "Temperature Logger";
 
             Installers.Add(processInstaller);
             Installers.Add(serviceInstaller);
